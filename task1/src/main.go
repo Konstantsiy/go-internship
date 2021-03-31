aec80583792694887ca5db6e06b68c498eaaca72
idi_obnimu <constintinforgit@gmail.com>
2021-03-31 21:46:05 +0300

    fix CapitalizeString function

--- task1/src/main.go
+++ task1/src/main.go
@@ -1,39 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
-
+// ReverseString to reverse a string.
 func ReverseString(str string) string {
 	runes := []rune(str)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
+
 	return string(runes)
 }
 
+// CapitalizeString to capitalizes a string.
 func CapitalizeString(str string) string {
 	runes := []rune(str)
-	startWordFlag := true
-	for i := 0; i < len(runes); i++ {
-		if runes[i] == ' ' {
-			startWordFlag = true
-		} else {
-			if ((runes[i] >= 'a' && runes[i] <= 'z') || (runes[i] >= 'а' && runes[i] <= 'я')) && startWordFlag {
-				runes[i] -= 32
-				startWordFlag = false
-			} else if ((runes[i] >= 'A' && runes[i] <= 'Z') || (runes[i] >= 'А' && runes[i] <= 'Я')) && !startWordFlag {
-				runes[i] += 32
-			} else {
-				startWordFlag = false
-			}
-		}
-	}
-	return string(runes)
-}
 
-func main() {
-	s := "_HELLO_ WorLd"
-	fmt.Println(ReverseString(s))
-	fmt.Println(CapitalizeString(s))
+	if (runes[0] >= 'a' && runes[0] <= 'z') || (runes[0] >= 'а' && runes[0] <= 'я') {
+		runes[0] -= 32
+	}
+
+	return string(runes)
 }
