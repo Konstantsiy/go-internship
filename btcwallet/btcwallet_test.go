0179a0d87d38a28d06362b0d40c167477b79a517
idi_obnimu <constintinforgit@gmail.com>
2021-04-07 23:49:15 +0300

    Add tests for Deposit and Withdraw methods

--- btcwallet/btcwallet_test.go
+++ btcwallet/btcwallet_test.go
@@ -1,34 +1,81 @@
 package btcwallet
 
 import (
-	"sync"
 	"testing"
-
-	_ "golang.org/x/tools/go/expect"
 )
 
-func TestBtcWallet(t *testing.T) {
-	wallet := NewBtcWallet(20)
-	wg := &sync.WaitGroup{}
-	var expected Bitcoin = 50
-	for i := 0; i < 3; i++ {
-		wg.Add(1)
-		go func(w *BtcWallet) {
-			defer wg.Done()
-			for j := 0; j < 2; j++ {
-				var err error
-				if err = w.Deposit(10); err != nil {
-					t.Errorf(err.Error())
-				}
-				if err = w.Withdraw(5); err != nil {
-					t.Errorf(err.Error())
-				}
-			}
-		}(wallet)
+func TestBtcWallet_Deposit(t *testing.T) {
+	const (
+		startBalance Bitcoin = 0
+		precision    int     = 2
+	)
+
+	testTable := []struct {
+		Source   []Bitcoin
+		Expected Bitcoin
+	}{
+		{
+			Source:   []Bitcoin{2.5, 20, 30, 45.5, 38},
+			Expected: 136,
+		},
+		{
+			Source:   []Bitcoin{12, -12.45, 10.45},
+			Expected: 22.45,
+		},
+		{
+			Source:   []Bitcoin{-12, -3.45, 0},
+			Expected: 0,
+		},
 	}
-	wg.Wait()
-	result := wallet.GetBalance()
-	if expected != result {
-		t.Errorf("Incorrect result. Expect %.2f, got %.2f", expected, result)
+
+	for _, tc := range testTable {
+		wallet := NewBtcWallet(startBalance)
+
+		for _, btcAmount := range tc.Source {
+			_ = wallet.Deposit(btcAmount)
+		}
+		result := wallet.GetBalance().RoundTo(precision)
+
+		if result != tc.Expected {
+			t.Errorf("Incorrect result. Expect %.2f, got %.2f", tc.Expected, result)
+		}
+	}
+}
+
+func TestBtcWallet_Withdraw(t *testing.T) {
+	const (
+		startBalance Bitcoin = 1000
+		precision    int     = 2
+	)
+
+	testTable := []struct {
+		Source   []Bitcoin
+		Expected Bitcoin
+	}{
+		{
+			Source:   []Bitcoin{0.34, 450, 500, 34.24, 10.2},
+			Expected: 5.22,
+		},
+		{
+			Source:   []Bitcoin{0, -12.45, 1000.01},
+			Expected: 1000,
+		},
+		{
+			Source:   []Bitcoin{999, 0.5, 0.49},
+			Expected: 0.01,
+		},
+	}
+
+	for _, tc := range testTable {
+		wallet := NewBtcWallet(startBalance)
+
+		for _, btcAmount := range tc.Source {
+			_ = wallet.Withdraw(btcAmount)
+		}
+		result := wallet.GetBalance().RoundTo(precision)
+
+		if result != tc.Expected {
+			t.Errorf("Incorrect result. Expect %.2f, got %.2f", tc.Expected, result)
+		}
 	}
 }
