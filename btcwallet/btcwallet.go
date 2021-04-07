b83409ea9ddd519248ab6a2edf64077fca3e0d38
idi_obnimu <constintinforgit@gmail.com>
2021-04-07 23:47:44 +0300

    Add RoundTo method for Bitcoin for correct testing

--- btcwallet/btcwallet.go
+++ btcwallet/btcwallet.go
@@ -1,13 +1,22 @@
+// Package btcwallet implements Bitcoins wallet for deposit, withdraw and get wallet's balance.
 package btcwallet
 
 import (
+	"errors"
+	"math"
 	"sync"
-
-	"github.com/pkg/errors"
 )
 
 // Bitcoin representation via the float64 data type.
 type Bitcoin float64
+
+// RoundTo truncates float64 type to a particular precision and return new Bitcoin.
+func (b Bitcoin) RoundTo(precision int) Bitcoin {
+	output := math.Pow(10, float64(precision))
+	number := float64(b) * output
+	rounded := int(number + math.Copysign(0.5, number))
+	return Bitcoin(float64(rounded) / output)
+}
 
 // BtcWallet implements a simple bitcoins wallet for deposit, withdraw and get current balance.
 type BtcWallet struct {
@@ -21,27 +30,27 @@ func NewBtcWallet(balance Bitcoin) *BtcWallet {
 }
 
 // Deposit increments the wallet balance by the given argument.
-func (wallet *BtcWallet) Deposit(data Bitcoin) error {
+func (wallet *BtcWallet) Deposit(amount Bitcoin) error {
 	wallet.Lock()
 	defer wallet.Unlock()
 
-	if data <= 0 {
-		return errors.New("the data for the deposit must be positive")
+	if amount <= 0 {
+		return errors.New("the Bitcoins amount for the deposit must be positive")
 	}
-	wallet.balance += data
+	wallet.balance += amount
 
 	return nil
 }
 
 // Withdraw decrements the wallet balance by the given argument.
-func (wallet *BtcWallet) Withdraw(data Bitcoin) error {
+func (wallet *BtcWallet) Withdraw(amount Bitcoin) error {
 	wallet.Lock()
 	defer wallet.Unlock()
 
-	if data <= 0 || data > wallet.balance {
-		return errors.New("the data for the withdraw must be limited by the balance")
+	if amount <= 0 || amount > wallet.balance {
+		return errors.New("the Bitcoins amount for the withdraw must be limited by the balance")
 	}
-	wallet.balance -= data
+	wallet.balance -= amount
 
 	return nil
 }
--- btcwallet/go.mod
+++ btcwallet/go.mod
@@ -1,8 +1,3 @@
 module github.com/Konstantsiy/go-internship/btcwallet
 
 go 1.13
-
-require (
-	github.com/pkg/errors v0.9.1
-	golang.org/x/tools v0.1.0
-)
