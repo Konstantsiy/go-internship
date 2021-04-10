14f03faa436bf261a15d1751d78af50ed944e07d
idi_obnimu <constintinforgit@gmail.com>
2021-04-10 20:08:09 +0300

    Fix tests, add helper function and test for concurrency

--- btcwallet/btcwallet_test.go
+++ btcwallet/btcwallet_test.go
@@ -2,8 +2,38 @@ package btcwallet
 
 import (
 	"errors"
+	"sync"
 	"testing"
 )
+
+func TestBtcWallet_Race(t *testing.T) {
+	var startBalance Bitcoin = 20
+	var expected Bitcoin = 50
+	wallet := NewBtcWallet(startBalance)
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func(w *BtcWallet) {
+			defer wg.Done()
+			for j := 0; j < 2; j++ {
+				var err error
+				if err = w.Deposit(10); err != nil {
+					t.Errorf(err.Error())
+				}
+				if err = w.Withdraw(5); err != nil {
+					t.Errorf(err.Error())
+				}
+			}
+		}(wallet)
+	}
+	wg.Wait()
+	result := wallet.GetBalance()
+
+	if expected != result {
+		t.Errorf("Incorrect result. Expect %.2f, got %.2f", expected, result)
+	}
+}
 
 func TestBtcWallet_Deposit(t *testing.T) {
 	const (
@@ -13,34 +43,34 @@ func TestBtcWallet_Deposit(t *testing.T) {
 	)
 
 	testTable := []struct {
-		Source      Bitcoin
-		Expected    Bitcoin
-		ExpectedErr error
+		SumToDeposit  Bitcoin
+		Expected      Bitcoin
+		ExpectedError error
 	}{
 		{
-			Source:      23.456,
-			Expected:    43.456,
-			ExpectedErr: nil,
+			SumToDeposit:  23.456,
+			Expected:      43.456,
+			ExpectedError: nil,
 		},
 		{
-			Source:      -0.234,
-			Expected:    startBalance,
-			ExpectedErr: errors.New(depositErrorMessage),
+			SumToDeposit:  -0.234,
+			Expected:      startBalance,
+			ExpectedError: errors.New(depositErrorMessage),
 		},
 	}
 
 	for _, tc := range testTable {
 		wallet := NewBtcWallet(startBalance)
 
-		if err := wallet.Deposit(tc.Source); err != nil {
-			if err.Error() != tc.ExpectedErr.Error() {
-				t.Error(err.Error())
+		if err := wallet.Deposit(tc.SumToDeposit); err != nil {
+			if err.Error() != tc.ExpectedError.Error() {
+				errorAssertionHelper(t, err, tc.ExpectedError)
 			}
-		} else {
-			result := wallet.GetBalance().RoundTo(precision)
-			if result != tc.Expected {
-				t.Errorf("Incorrect result. Expect %.3f, got %.3f", tc.Expected, result)
-			}
+		}
+		result := wallet.GetBalance().RoundTo(precision)
+
+		if result != tc.Expected {
+			t.Errorf("incorrect result. Expect %.3f, got %.3f", tc.Expected, result)
 		}
 	}
 }
@@ -53,39 +83,46 @@ func TestBtcWallet_Withdraw(t *testing.T) {
 	)
 
 	testTable := []struct {
-		Source      Bitcoin
-		Expected    Bitcoin
-		ExpectedErr error
+		SumToWithdraw Bitcoin
+		Expected      Bitcoin
+		ExpectedError error
 	}{
 		{
-			Source:      999.445,
-			Expected:    0.555,
-			ExpectedErr: nil,
+			SumToWithdraw: 999.445,
+			Expected:      0.555,
+			ExpectedError: nil,
 		},
 		{
-			Source:      -12.34,
-			Expected:    startBalance,
-			ExpectedErr: errors.New(withdrawErrorMessage),
+			SumToWithdraw: -12.34,
+			Expected:      startBalance,
+			ExpectedError: errors.New(withdrawErrorMessage),
 		},
 		{
-			Source:      1000.34,
-			Expected:    startBalance,
-			ExpectedErr: errors.New(withdrawErrorMessage),
+			SumToWithdraw: 1000.34,
+			Expected:      startBalance,
+			ExpectedError: errors.New(withdrawErrorMessage),
 		},
 	}
 
 	for _, tc := range testTable {
 		wallet := NewBtcWallet(startBalance)
 
-		if err := wallet.Withdraw(tc.Source); err != nil {
-			if err.Error() != tc.ExpectedErr.Error() {
-				t.Error(err.Error())
-			}
-		} else {
-			result := wallet.GetBalance().RoundTo(precision)
-			if result != tc.Expected {
-				t.Errorf("Incorrect result. Expect %.3f, got %.3f", tc.Expected, result)
+		if err := wallet.Withdraw(tc.SumToWithdraw); err != nil {
+			if err.Error() != tc.ExpectedError.Error() {
+				errorAssertionHelper(t, err, tc.ExpectedError)
 			}
 		}
+		result := wallet.GetBalance().RoundTo(precision)
+
+		if result != tc.Expected {
+			t.Errorf("incorrect result. Expect %.3f, got %.3f", tc.Expected, result)
+		}
+	}
+}
+
+func errorAssertionHelper(t *testing.T, givenError, expectedError error) {
+	t.Helper()
+	if givenError.Error() != expectedError.Error() {
+		t.Error("expected error but didn't get one")
 	}
 }
