98690871d59a6756d35a02eee32237d3f4eb1d7f
idi_obnimu <constintinforgit@gmail.com>
2021-04-20 11:44:26 +0300

    Fix algorithm.go

--- parentheses/algorithm/algorithm.go
+++ parentheses/algorithm/algorithm.go
@@ -1,8 +1,8 @@
-// Package algorithm implements function for checking the balancing of parentheses in a string.
+// Package algorithm checks whether the passed string is a balanced sequence of brackets.
 package algorithm
 
-// Map of buckets for comparison.
-var buckets = map[rune]rune{
+// Map of brackets for comparison.
+var brackets = map[rune]rune{
 	'(': ')',
 	'[': ']',
 	'{': '}',
@@ -10,8 +10,8 @@ var buckets = map[rune]rune{
 
 // IsBalanced verifies if the given string is a balanced sequence of brackets.
 func IsBalanced(str string) bool {
-	var runes = []rune(str)
 	var stack []rune
+	runes := []rune(str)
 
 	for _, r := range runes {
 		switch r {
@@ -19,7 +19,7 @@ func IsBalanced(str string) bool {
 			stack = append(stack, r)
 		case ')', ']', '}':
 			stackLength := len(stack)
-			if stackLength == 0 || r != buckets[stack[stackLength-1]] {
+			if stackLength == 0 || r != brackets[stack[stackLength-1]] {
 				return false
 			}
 			stack = stack[:stackLength-1]
